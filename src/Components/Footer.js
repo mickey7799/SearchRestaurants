02c638500bdafa859ab758706586deef40806f79
Mickey <vickeylee.w@gmail.com>
2020-06-08 15:01:05 +1000

    Add .env

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 
-export default class Footer extends Component{
+export default class Footer extends Component {
 
-    render(){
+    render() {
         return (
             <div id="Footer" className="footer">
                 <div className="footer-container w-container">
                     <div className="footer-wrapper-top">
                         <a href="/" className="w-inline-block w--current">
-                            <img src="/images/cupcake.svg" width="160" height="44" alt="" className="cake-logo"/>
-                            
-                             
+                            <img src="/images/cupcake.svg" width="160" height="44" alt="" className="cake-logo" />
+
+
                         </a>
                         <a href="mailto:hic.ch.ntu@gmail.com" className="link">Contact</a>
-                        <a  href="https://www.instagram.com/yelp/?hl=en" target="_blank" rel="noopener noreferrer" className="Footer__InstagramLink-sc-1mb098p-0 bBdLAF">
-                            <img src="/images/instagram.svg"/>
+                        <a href="https://www.instagram.com/yelp/?hl=en" target="_blank" rel="noopener noreferrer" className="Footer__InstagramLink-sc-1mb098p-0 bBdLAF">
+                            <img src="/images/instagram.svg" />
                         </a>
                         <div className="text-block">© 2020 Vickey Lee</div>
                     </div>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,23 +2,23 @@ import React, { Component } from 'react';
 
 export default class Header extends Component {
 
-    render(){
+    render() {
 
         return (
             <div data-collapse="none" data-animation="default" data-duration="400" className="navbar w-nav">
                 <div className="w-container">
-                    
-                    <img src="/images/mini-donut.svg" alt="alt_image" className=" w-nav-brand  h-logo"/>
-                    
+
+                    <img src="/images/mini-donut.svg" alt="alt_image" className=" w-nav-brand  h-logo" />
+
                     <nav role="navigation" className="w-nav-menu">
-                        <a  href= "https://www.yelp.com/" className="nav-link w-nav-link" style={{maxWidth: 940+'px'}}>Yelp</a>
-                        <a  href="https://www.instagram.com/yelp/?hl=en" target="_blank" rel="noopener noreferrer" className="Home__InstagramLink-sc-1sbsg8u-0 kCEMFk">
-                            <img src="/images/instagram.svg"/>
+                        <a href="https://www.yelp.com/" className="nav-link w-nav-link" style={{ maxWidth: 940 + 'px' }}>Yelp</a>
+                        <a href="https://www.instagram.com/yelp/?hl=en" target="_blank" rel="noopener noreferrer" className="Home__InstagramLink-sc-1sbsg8u-0 kCEMFk">
+                            <img src="/images/instagram.svg" />
                         </a>
                     </nav>
                     <div className="w-nav-button">
-                        <div className="w-icon-nav-menu"/>
-                    
+                        <div className="w-icon-nav-menu" />
+
                     </div>
                 </div>
                 <div className="w-nav-overlay" data-wf-ignore=""></div>
